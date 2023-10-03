355d241213435fa019a7b7d2f6150cde107bcd02
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-10-02 21:28:29 -0700

    update names

--- v1/display-v1.sh
+++ v1/display-v1.sh
@@ -62,9 +62,9 @@ def get_onion_address():
 def get_service_status():
     status = os.popen('systemctl is-active hush-line.service').read().strip()
     if status == 'active':
-        return '✔ Hush Line is running'
+        return '✔ Blackbox is running'
     else:
-        return '⨯ Hush Line is not running'
+        return '⨯ Blackbox is not running'
 
 def display_status(epd, status, onion_address, name, email, key_id, expires):
     print(f'Displaying status: {status}, Onion address: {onion_address}')
