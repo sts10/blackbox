056ac7d8518aa3ee11676935b48fec97854beff0
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-10-02 17:47:33 -0700

    add key

--- v1/qr-installer-beta.sh
+++ v1/qr-installer-beta.sh
@@ -81,7 +81,7 @@ def setup():
         smtp_server = request.form.get('smtp_server')
         password = request.form.get('password')
         smtp_port = request.form.get('smtp_port')
-        pgp_key_address = request.form.get('pgp_key_address')
+        pgp_public_key = request.form.get('pgp_public_key')
 
         # Save the configuration
         with open('/tmp/setup_config.json', 'w') as f:
@@ -90,10 +90,14 @@ def setup():
                 'smtp_server': smtp_server,
                 'password': password,
                 'smtp_port': smtp_port,
-                'pgp_key_address': pgp_key_address
+                'pgp_public_key': pgp_public_key
             }, f)
 
         setup_complete = True
+
+        # Save the provided PGP key to a file
+        with open('/home/hush/hushline/public_key.asc', 'w') as keyfile:
+            keyfile.write(pgp_public_key)
 
         return redirect(url_for('index'))
 
@@ -204,13 +208,9 @@ EMAIL=$(jq -r '.email' /tmp/setup_config.json)
 NOTIFY_SMTP_SERVER=$(jq -r '.smtp_server' /tmp/setup_config.json)
 NOTIFY_PASSWORD=$(jq -r '.password' /tmp/setup_config.json)
 NOTIFY_SMTP_PORT=$(jq -r '.smtp_port' /tmp/setup_config.json)
-PGP_KEY_ADDRESS=$(jq -r '.pgp_key_address' /tmp/setup_config.json)
 
 # Kill the Flask setup process
 pkill -f blackbox-setup.py
-
-# Download the public PGP key and rename to public_key.asc
-wget $PGP_KEY_ADDRESS -O $PWD/public_key.asc
 
 # Create a systemd service
 cat >/etc/systemd/system/hush-line.service <<EOL
